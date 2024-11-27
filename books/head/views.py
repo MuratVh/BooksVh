867ee3185e6e4060dd700da9fddfd3b6f5d401ff
MuratVh <murat-1434@mail.ru>
2024-11-27 17:32:52 +0300

    сделал добавление книги

--- books/head/views.py
+++ books/head/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 def home(request):
     books = Book.objects.all()
     return render(request, 'home.html', {'books': books})
+
+def create_book(request):
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save(commit=False)
+        Book.objects.create(title=form.data['title'], author=form.data['author'], year=form.data['year'], status=form.data['status'])
+        form.clean()
+        return redirect('head:home')
+    return render(request, 'create_book.html', {'form': form})
