fff22cbab7b44d531b89d81543475b59527f9698
MuratVh <murat-1434@mail.ru>
2024-11-27 15:53:49 +0300

    прописал форму BookForm

---
+++ books/head/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import *
+
+class BookForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(
+        attrs= {'class': 'input'}
+    ), label= 'Название книги')
+    author = forms.CharField(widget=forms.TextInput(
+        attrs= {'class': 'input'}
+    ), label= 'Имя автора')
+    year = forms.IntegerField(label='год издания')
+    status = forms.ChoiceField(label='выберите статус', choices=STATUS)
+    
+    class Meta:
+        model = Book
+        fields = [
+            'title',
+            'author',
+            'year',
+            'status',
+        ]
